61b1e8ca5cee661652e4146f5540ed0e6cbe1c8c
yitec <yitec@outlook.com>
2021-01-30 01:02:57 +0800

    将 sqlite 重新加入解决方案

---
+++ dotnet/WFlow.SqliteRepository/Repositories/SqliteActivityRepository.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SQLite;
+using System.Data.Common;
+using System.Data;
+
+namespace WFlow.Repositories
+{
+    public class SqliteFlowRepository: FlowRepository
+    {
+        public SqliteFlowRepository(SQLiteConnection connection) : base(connection) { }
+
+        public SqliteFlowRepository(string connectionString) : base(new SQLiteConnection(connectionString)) { 
+        }
+
+        protected override DbParameter CreateParameter(object value, string name = null,DbType? type=null)
+        {
+            var param = new SQLiteParameter
+            {
+                Value = value ?? DBNull.Value,
+                ParameterName = name
+            };
+            if (type!=null)param.DbType = type.Value;
+            return param;
+            
+        }
+    }
+}
---
+++ dotnet/WFlow.SqliteRepository/SqliteProgram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WFlow.Repositories
+{
+    class SqliteProgram
+    {
+        static readonly string ConnectionString = "Data Source=wf.db;Version=3;New=True;Compress=True;BinaryGUID=False;";
+        static void Main(string[] args)
+        {
+            Engine.Development = true;
+            var arepo = new SqliteFlowRepository(ConnectionString);
+            var factory = new Engine(null,arepo);
+            var user = new User("01","yiy");
+            //factory.StartFlow("sample.wf",null,user,null);
+        }
+    }
+}
